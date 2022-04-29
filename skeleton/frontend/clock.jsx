df23fc9216536498499c75b7871d8cc1fe6b96b9
Billy <billy.phan4@gmail.com>
2022-04-29 16:23:03 -0700

    start phase 3

--- skeleton/frontend/clock.jsx
+++ skeleton/frontend/clock.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 class Clock extends Component {
     constructor(props) {
@@ -15,16 +17,25 @@ class Clock extends Component {
     
     }
 
+    componentWillUnmount() {
+        clearInterval(() => {
+            this.currentTime
+        })
+    }
+
     tick() {
         this.setState({time: new Date()})
     }
 
     render() {
         return (
+            <>
+                <h1>Clock</h1>
             <div class='clock'>
-                <h1>Clock</h1> 
-                <p>{this.state.time.getHours()}:{this.state.time.getMinutes()}:{this.state.time.getUTCSeconds()}</p>
+                <h1>Time: {this.state.time.getHours()}:{this.state.time.getMinutes()}:{this.state.time.getUTCSeconds()}</h1>
+                <h1>Date: {days[this.state.time.getDay()]} {months[this.state.time.getMonth()]} {this.state.time.getDate()}, {this.state.time.getFullYear()}</h1>
             </div>
+            </>
         )
     }
 }
